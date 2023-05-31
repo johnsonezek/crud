a7089f975da95841ef144c9aefe8340f8113eca2
johnson <johnsonezek@gmail.com>
2023-05-31 13:04:24 +0530

    error commit

---
+++ src/components/Employees.js
@@ -0,0 +1,18 @@
+const Employees = [
+    {
+        id:"1",
+        Name:"Johnson Ezek",
+        age:"30"
+    },
+     {
+        id:"2",
+        Name:"Nandhini",
+        age:"29"
+    },
+     {
+        id:"3",
+        Name:"Sathish Kumar",
+        age:"28"
+    },
+]
+export default Employees;
